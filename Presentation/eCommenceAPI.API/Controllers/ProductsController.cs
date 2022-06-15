9db2dce550778741f9027b97eeae924ad5ff9f9f
Mahdi Guliyev <quliyev.mehdi.1997@gmail.com>
2022-06-15 18:36:28 +0400

    Making Upload Files API for Product (Capter: 25)

--- Presentation/eCommenceAPI.API/Controllers/ProductsController.cs
+++ Presentation/eCommenceAPI.API/Controllers/ProductsController.cs
@@ -12,11 +12,16 @@ namespace eCommenceAPI.API.Controllers
     {
         private readonly IProductWriteRepository _productWriteRepository;
         private readonly IProductReadRepository _productReadRepository;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public ProductsController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository)
+        public ProductsController(
+            IProductWriteRepository productWriteRepository, 
+            IProductReadRepository productReadRepository, 
+            IWebHostEnvironment webHostEnvironment)
         {
             _productWriteRepository = productWriteRepository;
             _productReadRepository = productReadRepository;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         [HttpGet]
@@ -56,6 +61,37 @@ namespace eCommenceAPI.API.Controllers
             });
             await _productWriteRepository.SaveAsync();
             return StatusCode((int)HttpStatusCode.Created);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            await _productWriteRepository.RemoveAsync(id);
+            await _productWriteRepository.SaveAsync();
+
+            return Ok(new
+            {
+                message = "Product is deleted successfully."
+            });
+        }
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Upload()
+        {
+            var uploadedPath = Path.Combine(_webHostEnvironment.WebRootPath, "resource/product-images");
+
+            if (!Directory.Exists(uploadedPath))
+                Directory.CreateDirectory(uploadedPath);
+
+            Random random = new();
+            foreach (var file in Request.Form.Files)
+            {
+                string fullPath = Path.Combine(uploadedPath, $"{random.Next()}{Path.GetExtension(file.FileName)}");
+
+                using FileStream fileStream = new (fullPath, FileMode.Create, FileAccess.Write, FileShare.None, 1024 * 1024, useAsync: false);
+                await file.CopyToAsync(fileStream);
+                await fileStream.FlushAsync();
+            }
+
+            return Ok();
         }
     }
 }
